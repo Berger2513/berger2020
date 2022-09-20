4ea3e00d2a2ea8558f7d9defc1f6ae733747c7f6
berger <15757591513@163.com>
2022-09-20 14:45:22 +0800

    edit

--- app/Http/Controllers/Admin/GoodsController.php
+++ app/Http/Controllers/Admin/GoodsController.php
@@ -9,9 +9,9 @@ class GoodsController extends Controller
 {
         public function upload(Request $request)
         {
-            $files = $request->file('file');
+            
 
-            $path = $request->file('avatar')->store('avatars','upyun');
+            $path = $request->file('file')->store('image','upyun');
 
             $pixfix = 'http://bela-goods.test.upcdn.net/';
             return $pixfix.$path;
