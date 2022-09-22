3b90d706dfdb093e320d5e04e0e054fdf85188c6
berger <15757591513@163.com>
2022-09-22 16:52:06 +0800

    跨域问题

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,16 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        header('Content-Type: text/html;charset=utf-8');
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods:POST,GET,PUT,OPTIONS,DELETE'); // 允许请求的类型
+        header('Access-Control-Allow-Credentials: true'); // 设置是否允许发送 cookies
+        header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Origin,Access-token,Content-Length,Accept-Encoding,X-Requested-with, Origin,Access-Control-Allow-Methods'); // 设置允许自定义请求头的字段
 
-        $response->header('Access-Control-Allow-Origin', '*');
-
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, multipart/form-data, application/json');
-
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-
-        $response->header('Access-Control-Allow-Credentials', 'false');
-
-        return $response;
+        return $next($request);
     }
 }
