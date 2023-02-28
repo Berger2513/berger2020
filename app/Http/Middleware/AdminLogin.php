39d4ec4dad3e37ecf00ed7e4854570305846fc00
berger <15757591513@163.com>
2023-02-28 10:21:28 +0800

    edit

--- app/Http/Middleware/AdminLogin.php
+++ app/Http/Middleware/AdminLogin.php
@@ -15,16 +15,22 @@ class AdminLogin
      */
     public function handle($request, Closure $next)
     {
-
+        $admin_arr = array();
         $user_root = Cache::get('admin_root');
         $user_admin = Cache::get('admin_admin');
         $user_bela = Cache::get('admin_bela_tempo');
+
+
+array_push($admin_arr,$user_root );
+array_push($admin_arr,$user_admin );
+array_push($admin_arr,$user_bela );
+
 
         if(!$request->token) {
             return response()->json(['code' => 401,'msg' => '请先登录用户']);
         }
 
-        if($user_root !=  $request->token  ||  $request->token  != $user_admin || $request->token  != $user_bela)
+        if(!in_array($request->token, $admin_arr))
         {
 
             return response()->json(['code' => 499,'msg' => '数据不匹配']);
